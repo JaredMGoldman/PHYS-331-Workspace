592268e771ae4c1884e8ea5ae58de2c9b028e175
Jared <jaredgoldman77@gmail.com>
2018-09-05 17:08:22 -0400

    Helping out Kaylee

--- Homework/Homework 2/HW_Helper.py
+++ Homework/Homework 2/HW_Helper.py
@@ -1,14 +1,4 @@
 import numpy as np
-def calcSum_64bit(delta):
-    sum = np.float64(0.0)
-    delta = np.float64(delta)  # Convert delta to a 64-bit floating point number.
-
-    if delta == np.float64(0.0):  # If the 64-bit representation is exactly zero, throw an error.
-        print("Error: delta = " + str(delta) + " is equal to zero at this precision of 64 bits.")
-        return
-
-    for i in range(0, int(round(1.0 / delta))):  # Compute the sum using a for loop.
-        sum += delta
-    return sum
-
-print(calcSum_64bit(10**-308))
+x=0.0000012346583937
+print (x)
+print (np.float16(x))
