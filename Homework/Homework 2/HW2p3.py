886940803b18e0c2a1cd40fe61f8457999f43170
Jared <jaredgoldman77@gmail.com>
2018-09-07 13:24:11 -0400

    Function to plot error in root guess/iteration

--- Homework/Homework 2/HW2p3.py
+++ Homework/Homework 2/HW2p3.py
@@ -1,18 +1,6 @@
-"""
-3. For each of the functions f1(x), f2(x) and f3(x) of problem 2, compute the sequence of 
-xmid and fmid values using a tolerance of 10-12. Make a plot of each (xmid, fmid) over the 
-plot of the function itself. Use appropriate marker styles to visualize the individual data 
-points of (xmid, fmid).
-
-4. For each function, compute the error at each iteration by assuming the root at the final 
-iteration is the “true” value. For the purpose of diagnostics, do not take the absolute value 
-– just define error as the difference of the root at a given iteration from the “true” value. 
-Display plots of this error versus the iteration number.
-"""
-
 import numpy as np
 import matplotlib.pyplot as plt
-
+from copy import deepcopy
 def f2(x):
     return x**3
 
@@ -33,7 +21,7 @@ def bracket(lo, hi):
     """
     return ((lo+hi)/2)
 
-def PerryThePlotapus(f,x,xlo,xhi,xtol):
+def PerryThePlotapus(f,x,xlo,xhi):
     """
     INPUT:
         f: function, this is the function graphed
@@ -53,12 +41,15 @@ def PerryThePlotapus(f,x,xlo,xhi,xtol):
     x_vals=np.arange(xlo,xhi+dx,dx)
     y_vals= f(x_vals)
     plt.scatter(x_array,y_array, color= "black")
-    plt.plot(x_vals,y_vals)
+    plt.plot(x_vals,y_vals, color = "orange")
     plt.grid()
-    plt.title("Problem 3, Function: " + f.__name__)
+    plt.title("Problem 3: Function " + f.__name__)
     plt.xlabel('x')
+    plt.axhline(color='black')
+    plt.axvline(color='black')
     plt.ylabel('y')
     plt.show()
+    errorGopher(f, x_array)
     return (x_array, y_array)
 
 
@@ -75,7 +66,6 @@ def rf_bisect(f,xlo,xhi,xtol,nmax):
     RETURNS: (tuple(float, int)) A root of f that meets the tolerance tol the number 
     of iteratons required to converge.
     """
-    from copy import deepcopy
     x_array=[]
     iters=0
     low=deepcopy(xlo)
@@ -92,11 +82,39 @@ def rf_bisect(f,xlo,xhi,xtol,nmax):
             if abs((0-f(bracket(low,high))))<= xtol:
                 root=float(bracket(low,high))
                 x_array.append(root)
-                return PerryThePlotapus(f,x_array,xlo,xhi,xtol)
+                return PerryThePlotapus(f,x_array,xlo,xhi)
         return "Iteration limit reached, no root found."
     except ZeroDivisionError:
         print("Function", f.__name__, "has no root.")
+
+def errorGopher(f,xvals):
+    """
+    INPUT:
+        f: function, one-dimensional
+        xvals: array, x values used in bisection root search
+    
+    OUTPUT:
+        graph of error as a function of iteration number
+    """
+    x_array = xvals
+    true_val = x_array[-1]
+    errorList=[]
+    iterList=[]
+    iter=0
+    for x in x_array:
+        iter+=1
+        errorList.append(true_val-x)
+        iterList.append(iter)
+    plt.plot(iterList,errorList, color="orange")
+    plt.title("Error as a Function of Iteration: Function " + f.__name__)
+    plt.xlabel('Iterations')
+    plt.ylabel('Error Values')
+    plt.axhline(color="black")
+    plt.axvline(color="black")
+    plt.grid()
+    plt.show()
+
 for f in [f2,f3,f4]:
     print("For function " + f.__name__)
-    rf_bisect(f,-1.,1., 1e-12, 1e5)
     print("")
+    rf_bisect(f,-1.,1., 1e-12, 1e5)
