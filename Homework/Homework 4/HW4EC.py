882335db49873558c9834aef89e706a18bd50884
Jared <jaredgoldman77@gmail.com>
2018-09-24 11:24:39 -0400

    Final touches and comments on code

--- Homework/Homework 4/HW4EC.py
+++ Homework/Homework 4/HW4EC.py
@@ -1,28 +1,46 @@
+# Modules used
+
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Function definition section
+
+# One-Dimensional Graphing Functions
+
 
 def g(x):
-    return x-(np.power(x,5)-3*np.power(x,3)+15*np.power(x,2)+27*x+9)/(5*np.power(x,4)-9*np.power(x,2)+30*x+27)
+	"""
+	Newton-Raphson fixed-point function
+	"""
+	return x-(np.power(x,5)-3*np.power(x,3)+15*np.power(x,2)+27*x+9)/(5*np.power(x,4)-9*np.power(x,2)+30*x+27)
 
 
 def dg(x):
-    return 1-(np.power((5*np.power(x,4)-9*np.power(x,2)+30*x+27),2)-(np.power(x,5)-3*np.power(x,3)+15*np.power(x,2)+27*x+9)*(20*np.power(x,3)-18*x+30))/(np.power((5*np.power(x,4)-9*np.power(x,2)+30*x+27),2))
+	"""
+	derivative
+	"""
+	return 1-(np.power((5*np.power(x,4)-9*np.power(x,2)+30*x+27),2)-(np.power(x,5)-3*np.power(x,3)+15*np.power(x,2)+27*x+9)*(20*np.power(x,3)-18*x+30))/(np.power((5*np.power(x,4)-9*np.power(x,2)+30*x+27),2))
 
 
 def dg_abs(x):
-    return np.abs(1-(np.power((5*np.power(x,4)-9*np.power(x,2)+30*x+27),2)-(np.power(x,5)-3*np.power(x,3)+15*np.power(x,2)+27*x+9)*(20*np.power(x,3)-18*x+30))/(np.power((5*np.power(x,4)-9*np.power(x,2)+30*x+27),2)))
+	"""
+	absolute value of the derivative
+	"""
+	return np.abs(1-(np.power((5*np.power(x,4)-9*np.power(x,2)+30*x+27),2)-(np.power(x,5)-3*np.power(x,3)+15*np.power(x,2)+27*x+9)*(20*np.power(x,3)-18*x+30))/(np.power((5*np.power(x,4)-9*np.power(x,2)+30*x+27),2)))
+
+
+# Additional Functions to be Plotted
 
 
 def diagonal(x):
     return x
 
 
-def horizontal(x):
+def horizontal(x):				
     return 1+0*x
 
 
-def HarryPlotter(f,func_num):
+def HarryPlotter(f):
     """
     "The Chosen function"
 
@@ -34,31 +52,32 @@ def HarryPlotter(f,func_num):
     OUTPUT:
         graph of function f over domain xlo-xhi with mesh size dx (definied within the function)
     """                                         
-    xlo, xhi, ylo, yhi, dx = -5, 0, -3, 2, 1e-3                  #Plot generator function
-    x_vals=np.arange(xlo,xhi+dx,dx)                #Determine x and y values
+    xlo, xhi, ylo, yhi, dx = -10, 10, -10, 10, 1e-3                     # Plot generator function
+    x_vals=np.arange(xlo,xhi+dx,dx)                                     # Determine x and y values
     y_vals= f(x_vals)
-    plt.plot(x_vals,y_vals, label=f.__name__,linestyle='o')
+    plt.plot(x_vals,y_vals, label=f.__name__)
     plt.ylim(ylo,yhi)
     plt.grid()
     plt.legend(loc=0)
     plt.axhline(color='black')
     plt.axvline(color='black')
-    plt.title("Problem 1: Function "+ 'g'+str(func_num))
+    plt.title("HW4 Extra Credit")
     plt.xlabel('x')
     plt.ylabel('y')
 
 
-def funcshinup(funcs):
-    i=0
-    for packet in funcs:
-        i+= 1
-        func_num=i
-        for f in packet:
-            HarryPlotter(f,func_num)
-        plt.show()
+def plotcaller1(funcs):
+	"""
+	INPUT:
+		funcs: list of functions, each function wished to be graph in increasing numerical order
+	OUTPUTS:
+		subplot of all functions from the funcs list
+	"""
+	for f in funcs:
+		HarryPlotter(f)
+	plt.show()
 
 
-funcs =[ (g, dg, dg_abs, diagonal, horizontal) ]
+funcs =[ g, dg, dg_abs, diagonal, horizontal ]
 
-
-funcshinup(funcs)
+plotcaller1(funcs)
--- Homework/Homework 4/HW4p1.py
+++ Homework/Homework 4/HW4p1.py
@@ -1,42 +1,69 @@
+# Modules Used
 import numpy as np
 import matplotlib.pyplot as plt
 from copy import deepcopy
 
+# Proprietary function
+
 def f(x):
     return np.power(x,5)-3*np.power(x,3)+15*np.power(x,2)+27*x+9
+
+
+# Primary Functions 
+
 
 def g1(x):
     return np.power(x,5)-3*np.power(x,3)+15*np.power(x,2)+28*x+9
 
+
 def g2(x):
     return -np.power(x,5)+3*np.power(x,3)-15*np.power(x,2)-26*x-9
+
 
 def g3(x):
     return (np.power(x,5)-3*np.power(x,3)+15*np.power(x,2)+25*x+9)/(-2)
 
+
+# Derivitives
+
+
 def dg1(x):
     return 5*np.power(x,4)-9*np.power(x,2)+30*x+27
+
 
 def dg2(x):
     return -5*np.power(x,4)+9*np.power(x,2)-30*x-26
 
+
 def dg3(x):
     return (5*np.power(x,4)-9*np.power(x,2)+30*x+25)/(-2)
 
-def diagonal(x):
-    return x
 
-def horizontal(x):
-    return 1+0*x
+# Absolute Value Functions of Derivatives
+
 
 def dg1_abs(x):
     return np.abs(5*np.power(x,4)-9*np.power(x,2)+30*x+27)
 
+
 def dg2_abs(x):
     return np.abs(-5*np.power(x,4)+9*np.power(x,2)-30*x-26)
 
+
 def dg3_abs(x):
     return np.abs((5*np.power(x,4)-9*np.power(x,2)+30*x+25)/(-2))
+
+
+# Additional Functions to be Plotted
+
+
+def diagonal(x):
+    return x
+
+
+def horizontal(x):
+    return 1+0*x
+
 
 def HarryPlotter(f,xlo,xhi,func_num):
     """
@@ -64,10 +91,17 @@ def HarryPlotter(f,xlo,xhi,func_num):
     plt.ylabel('y')
 
 #----------------------------
-#Part b
+# Part b
 
-def funcshinup(funcs,xlo,xhi):
-    i=0
+
+def plotcaller0(funcs,xlo,xhi):
+    """
+    INPUT:
+        funcs: list of tuples, each function wished to be graph in increasing numerical order
+    OUTPUTS:
+        subplot of all functions from the funcs list
+    """
+	i=0
     for packet in funcs:
         i+= 1
         func_num=i
@@ -75,48 +109,59 @@ def funcshinup(funcs,xlo,xhi):
             HarryPlotter(f,xlo,xhi,func_num)
         plt.show()
 
-# xlo, xhi, funcs = -10, 10, [(g1,dg1,dg1_abs,diagonal,horizontal),(g2,dg2,dg2_abs,diagonal,horizontal),(g3,dg3,dg3_abs,diagonal,horizontal)]
+xlo, xhi, funcs = -10, 10, [(g1,dg1,dg1_abs,diagonal,horizontal),(g2,dg2,dg2_abs,diagonal,horizontal),(g3,dg3,dg3_abs,diagonal,horizontal)]
 
-# funcshinup(funcs, xlo, xhi)
+plotcaller0(funcs, xlo, xhi)
 
 #-----------------------------
-#Part c
+# Part c
+
 
 def fixed_pt(g,xstart,tol,nmax):
-    x0=xstart
-    x= deepcopy(x0)
-    i=0
-    #error_vals=np.array([])
-    while i<=nmax:
-        i+=1
-        x = g(x0)
-        #error_vals=np.append(error_vals, np.abs(x-x0))
-        if abs(x-x0)<=tol:
-            #PlottingAlong(error_vals)
-            return x
-        #print('x:',x)
-        x0= deepcopy(x)
-    return "Iteration limit exceeded"
+    try:
+        x0=xstart                                               # Variable definition
+        x= deepcopy(x0)
+        i=0
+        error_vals=np.array([])
+        while i<=nmax:                                          # Set iteration limit
+            i+=1
+            x = g(x0)
+            error_vals=np.append(error_vals, np.abs(x-x0))
+            if abs(x-x0)<=tol:                                  # Check percision of x value
+                error_v_iter(error_vals)
+                return x
+            x0= deepcopy(x)
+        return "Iteration limit exceeded"                       # error plotting function not used in my code
+    except ZeroDivisionError:
+        print("An error has occured. Please try another function.")
 
-# def PlottingAlong(error):
-#     """
-#     #INPUT:
-#     #error: numpy array, values of error
-#     """                                                 #Plot generator function
-#     x_vals=np.arange(1,len(error)+1)                #Determine x and y values
-#     y_vals=error
-#     plt.plot(x_vals,y_vals)
-#     plt.grid()
-#     plt.axhline(color='black')
-#     plt.axvline(color='black')
-#     plt.title("Error vs. Iteration")
-#     plt.xlabel('x')
-#     plt.ylabel('y')
-#     plt.yscale('log')
-#     plt.show()
+def error_v_iter(error):
+    """
+    INPUT:
+        error: numpy array, values of error per iteration
+    OUTPUT:
+        graph of error a function of iteration number
+    """                                                         # Plot generator function
+    x_vals=np.arange(1,len(error)+1)                          	# Determine x and y values
+    y_vals=error
+    plt.plot(x_vals,y_vals)
+    plt.grid()
+    plt.axhline(color='black')
+    plt.axvline(color='black')
+    plt.title("Error vs. Iteration")
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.yscale('log')
+    plt.show()
 
-def CodeOfCalling(nmax):
-    tol=1e-6
+def CodeOfCalling(nmax, tol):
+    """
+    INPUT:
+        nmax: int, number of max iterations for root finding functions below
+        tol: float or int, tolerance of root finding functions
+    OUTPUT:
+        str: For function: < name > : < root or error message >
+    """
     for g in [g1,g2,g3]:
         if g==g1:
             for xstart in [-0.77, -1.86]:
@@ -128,4 +173,6 @@ def CodeOfCalling(nmax):
             for xstart in [-0.69, -1.87]:
                 print('For function',g.__name__,':',str(fixed_pt(g,xstart,tol,nmax)))
 
-CodeOfCalling(30)
+nmax, tol = 30, 1e-15
+
+CodeOfCalling(nmax, tol)
